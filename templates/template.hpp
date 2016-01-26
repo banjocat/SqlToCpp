00fb61540f16801a1ac938017dabcb6afd9cb02c
banjocat <jackmuratore@gmail.com>
2016-01-25 19:00:20 -0500

    Remove full API
    Stick to workong on what is already impelemented
    Refactor before adding new feature

--- templates/template.hpp
+++ templates/template.hpp
@@ -28,63 +28,12 @@ namespace {{database_name}}
         {% endfor %}
 
         static int sqlCallback(void *pArg, int argc, char **text, char **columnName);
-        std::string CreateSql(const std::vector<std::string>& createColumns = std::vector<std::string>());
-        std::string UpdateSql(
-                const std::vector<std::string>& whereColumns,
-                const std::vector<std::string>& updateColumns = std::vector<std::string>());
-        std::string ReadSql(
-                const std::vector<std::string>& whereColumns = std::vector<std::string());;
-        std::string DeleteSql(
-                const std::vector<std::string>& whereColumns = std::vector<std::string());;
     };
+
 
 
 
     
-    std::string CreateSql()
-    {
-        stringstream sql;
-
-        sql << "INSERT INTO " << {{name}};
-        if (createColumns.empty())
-        {
-            sql << " VALUES (";
-            {% for column in table.columns %}
-            sql << "'" << {{column.name}} << "',"
-            {% endfor %}
-            string removeComma(sql.str());
-            removeComma.pop_back();
-            sql.str(removeComma);
-            sql << ");";
-        }
-        else 
-        {
-            sql << "(";
-            for (auto column : createColumns)
-            {
-                sql << column << ",";
-            }
-            string removeComma(sql.str());
-            removeComma.pop_back();
-            sql.str(removeComma);
-            sql << ");";
-            {% for column in table.columns %}
-            for (auto column : createColumns)
-            {
-                if (stringtoupper(column) == stringtoupper("{{column}}")
-                    sql << "'" << {{column}} << "',"
-            }
-            {% endfor %}
-        }
-
-        return sql.str();
-    };
-
-    std::string UpdateSql(const std::string& wherColumns)
-    {
-    }
-
-    {% endfor %}
 };
 
 #endif
